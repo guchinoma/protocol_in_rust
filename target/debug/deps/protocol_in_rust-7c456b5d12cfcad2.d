c1902abd5f57a0f42478d4bd96e0969b7ca52246
guchinoma <sorgens97chrome@yahoo.co.jp>
2024-03-20 22:34:46 +0900

    set up README

---
+++ target/debug/deps/protocol_in_rust-7c456b5d12cfcad2.d
@@ -0,0 +1,5 @@
+/Users/KeiNomaguchi/protocol_in_rust/target/debug/deps/libprotocol_in_rust-7c456b5d12cfcad2.rmeta: src/main.rs
+
+/Users/KeiNomaguchi/protocol_in_rust/target/debug/deps/protocol_in_rust-7c456b5d12cfcad2.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/protocol_in_rust-fbd0d733aac7a079.d
@@ -0,0 +1,5 @@
+/Users/KeiNomaguchi/protocol_in_rust/target/debug/deps/libprotocol_in_rust-fbd0d733aac7a079.rmeta: src/main.rs
+
+/Users/KeiNomaguchi/protocol_in_rust/target/debug/deps/protocol_in_rust-fbd0d733aac7a079.d: src/main.rs
+
+src/main.rs:
