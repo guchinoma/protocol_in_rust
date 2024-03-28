cf8bfc8b30fd72d8062078af7a9acc67239c912b
guchinoma <sorgens97chrome@yahoo.co.jp>
2024-03-28 17:28:01 +0900

    set up network config sh script. Only run in virtual env

---
+++ network_setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#
+#
+# 注意！ 仮想環境以外でこれを実行させるな。面倒臭いことになる
+#
+#
+
+echo "注意!　仮想環境以外で動かすな"
+echo "仮想環境でこれ実行させると、ネットワーク設定を直す必要がある。"
+echo "仮想環境だと確認していますか？"
+echo "確認しているならyesと打つ"
+
+echo "Caution! Do not run this sh in non-virtual environment."
+echo "If you run this, you need to fix network setting of your laptop, which is troublesome."
+echo "Do you recognize your env is now virtual environment?"
+echo "If you do, say yes."
+
+read -p ":" INPUT_STR
+
+if [$INPUT_STR != "yes"]; then 
+  exit 0
+fi
+
+echo "Go on. r u sure?"
+echo "続きます。承知していますか？"
+
+
+read -p ":" INPUT_STR
+
+if [$INPUT_STR != "yes"]; then 
+  exit 0
+fi
+
+sudo ip tuntap add mode tap user $USER name tap0
+sudo ip addr add 192.0.2.1/24 dev tap0
+sudo ip link set tap0 up
