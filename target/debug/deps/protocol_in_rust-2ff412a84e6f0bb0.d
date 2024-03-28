4fd617ecfd5bf101d7ef6cc6eea339c3001bdf81
guchinoma <sorgens97chrome@yahoo.co.jp>
2024-03-28 11:55:25 +0900

    clean target

--- target/debug/deps/protocol_in_rust-2ff412a84e6f0bb0.d
+++
@@ -1,8 +0,0 @@
-/Users/KeiNomaguchi/protocol_in_rust/target/debug/deps/libprotocol_in_rust-2ff412a84e6f0bb0.rmeta: src/lib.rs src/device.rs src/nif.rs src/socket.rs
-
-/Users/KeiNomaguchi/protocol_in_rust/target/debug/deps/protocol_in_rust-2ff412a84e6f0bb0.d: src/lib.rs src/device.rs src/nif.rs src/socket.rs
-
-src/lib.rs:
-src/device.rs:
-src/nif.rs:
-src/socket.rs:
