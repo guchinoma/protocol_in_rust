938563b13fd41dbad5ff701af5d9e6d2c46baced
guchinoma <sorgens97chrome@yahoo.co.jp>
2024-03-28 21:21:32 +0900

    fixed

--- network_setup.sh
+++ network_setup.sh
@@ -9,28 +9,20 @@
 echo "注意! 仮想環境以外で動かすな"
 echo "仮想環境でこれ実行させると、ネットワーク設定を直す必要がある。"
 echo "仮想環境だと確認していますか？"
-echo "確認しているならyesと打つ"
+echo "確認しているならyと打つ"
 
 echo "Caution! Do not run this sh in non-virtual environment."
 echo "If you run this, you need to fix network setting of your laptop, which is troublesome."
 echo "Do you recognize your env is now virtual environment?"
-echo "If you do, say yes."
+echo "If you do, say"
+echo "y"
 
-read -p INPUT_STR
+read -p ":" INPUT_STR
 
-if ["$INPUT_STR" != "yes"]; then 
-  exit 1
-fi
-
-echo "Go on. r u sure? If so. say yes"
-echo "続きます。承知していますか？ よければyes"
-
-
-read -p INPUT_STR
-
-if [[$INPUT_STR != "yes"]]; then 
-  exit 1
-fi
+case "$INPUT_STR" in
+  [yY]) echo "go on" ;;
+  *) exit 1;;
+esac
 
 sudo ip tuntap add mode tap user $USER name tap0
 sudo ip addr add 192.0.2.1/24 dev tap0
